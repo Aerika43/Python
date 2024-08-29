0a9681c86166b6d2173022b99760b72ddd3fdba3
Aerika Talaviya <aerikatalaviya@gmail.com>
2024-08-29 12:45:17 +0530

    Add files via upload

---
+++ flag.py
@@ -0,0 +1,6 @@
+for i in range(1,8):
+    for j in range(1,i+1):
+        print(' * ',end=' ')
+    print()
+for k in range(1,5):
+    print(' * ')
---
+++ multiplenum_whileloop.py
@@ -0,0 +1,10 @@
+a = int(input('Enter a Number: '))     
+while (a != 0):                             
+    digit = a % 10                        
+    m = digit
+    
+    dig1 = m % 10            
+    n = dig1                      
+
+    mul = m*n
+print(mul)
